69538dc1d91214f7f081786b5e0915846a139ac5
mm0202 <for_etc@hotmail.com>
2020-06-06 15:06:24 +0900

    SB-344 add eslint, prettier, stylelint, jest, enzyme, cypress, styled-components, etc.

--- src/Welcome.stories.tsx
+++ src/Welcome.stories.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { linkTo } from '@storybook/addon-links';
-import { Button } from "@storybook/react/demo";
+import React from 'react'
+import { linkTo } from '@storybook/addon-links'
+import { Button } from '@storybook/react/demo'
 
 export default {
-    title: 'Welcome'
-};
+  title: 'Welcome',
+}
 
-export const ToStorybook = () => <Button onClick={linkTo('Sample')}>to Sample</Button>
+export const ToStorybook = (): React.ReactElement => (
+  <Button onClick={linkTo('Sample')}>to Sample</Button>
+)
 
 ToStorybook.story = {
-    name: 'to Storybook',
+  name: 'to Storybook',
 }
